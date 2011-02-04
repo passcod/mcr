8ccf09502b84ebe4a9b00fb119be84e93e06da6b
passcod <passcod3@gmail.com>
2011-02-04 20:06:23 +1300

    Added horizontal navigation.

--- init.js
+++ init.js
@@ -20,14 +20,9 @@ $(function (){
 	MCR.Get.chapter();
 	
 	alert(""+
-	  "This is a pre-release (beta) version. Check often for updates.\n\nAs such, features may be broken or missing. "+
-	  "Please do not report these as bugs. Refer to the release notes below:"+
-	  "\n\n"+
-	  "* No cache.\n"+
-	  "* Not possible to change options.\n"+
-    "* Icons for nav panel.\n"+
-    "* Faster load, lighter footprint.\n"+
-    "* Fetches and displays manga description and details.\n"+
-    "* No navigation yet. Have to browse the site to select manga *and* chapters.\n"
+	  "This is a pre-release (beta) version. "+
+	  "As such, features may be broken or missing. "+
+	  "Please do not report these as bugs."+
+	  "Check often for updates. "
 	);
 });
