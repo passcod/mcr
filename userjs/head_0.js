ab6dc8b925f7b8b0e1a39bbcd1c71fca26c6b0fe
Félix Saparelli <me@passcod.net>
2011-09-28 00:50:23 +1300

    Simple page with iframe to stats

--- userjs/head_0.js
+++
@@ -1,5 +0,0 @@
-// ==UserScript==
-// @name           MR.net Chapter Reader
-// @description    Displays full chapters from MR.net in a simple interface.
-// @author         Félix Saparelli
-// @license        MIT
