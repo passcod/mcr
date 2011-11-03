62de05facf94299a02a22c43037b9b180ab5b899
Félix Saparelli <me@passcod.net>
2011-11-04 00:57:26 +1300

    Final

--- lib/json.php
+++
@@ -1,3 +0,0 @@
-<?php
-  echo json_encode(file_get_contents($argv[1]));
-?>
--- userjs/meta.js
+++
@@ -1,8 +0,0 @@
-// ==UserScript==
-// @name           MR.net Chapter Reader
-// @description    Displays full chapters from MR.net in a simple interface.
-// @author         Félix Saparelli
-// @license        MIT
-// @namespace      http://passcod.net/mcr
-// @include        http://www.mangareader.net/*
-// @include        http://mangareader.net/*
